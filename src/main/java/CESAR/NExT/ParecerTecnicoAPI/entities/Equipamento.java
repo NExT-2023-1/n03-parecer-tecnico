e9718ced5c9c48f009b7349386c72d4260dc9c54
Eduardo Morais <84351592+EduardoVGVM@users.noreply.github.com>
2023-09-14 20:06:29 -0300

    Update Equipamento.java

--- src/main/java/CESAR/NExT/ParecerTecnicoAPI/entities/Equipamento.java
+++ src/main/java/CESAR/NExT/ParecerTecnicoAPI/entities/Equipamento.java
@@ -1,14 +1,10 @@
 package CESAR.NExT.ParecerTecnicoAPI.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,9 +25,4 @@ public class Equipamento {
     private String defeito;
     @Column(nullable = false)
     private String parecer;
-
-    @ManyToOne
-    @JoinColumn(name="cliente_id", nullable = false)
-    @JsonBackReference
-    private Cliente cliente;
 }
