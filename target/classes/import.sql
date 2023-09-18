7dc6cf4877197a0ea329a5a584c275c86b202182
Eduardo Morais <dudu.vgvm@gmail.com>
2023-09-18 20:50:12 -0300

    Removendo Import.sql

--- target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_equipamentos(id_equipamento, tipo, defeito,parecer) VALUES (1, 'celular', 'tela queimada', 'trocar a tela toda');
-INSERT INTO tb_equipamentos(id_equipamento, tipo, defeito,parecer) VALUES (2, 'notebook', 'teclado quebrado', 'sem reparo');
-
-
-INSERT INTO tb_clientes(id, nome, cpf,telefone) VALUES (1, 'Lucas', '08888888','99999999');
