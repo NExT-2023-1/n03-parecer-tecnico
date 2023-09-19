5e85d1ebc2ce27de78c1290a9f0cd66269463b50
Luiz Diniz <luizdinizmedeiros@gmail.com>
2023-09-19 19:54:18 -0300

    equipamentoDTO

--- src/main/java/CESAR/NExT/ParecerTecnicoAPI/dto/EquipamentoDTO.java
+++ src/main/java/CESAR/NExT/ParecerTecnicoAPI/dto/EquipamentoDTO.java
@@ -1,7 +1,52 @@
 package CESAR.NExT.ParecerTecnicoAPI.dto;
+import CESAR.NExT.ParecerTecnicoAPI.entities.Equipamento;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class EquipamentoDTO {
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private long id;
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String tipo;
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String defeito;
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String parecer;
+
+    public Equipamento toEntity() {
+        return Equipamento.builder()
+                .id(this.id)
+                .tipo(this.tipo)
+                .defeito(this.defeito)
+                .parecer(this.parecer)
+                .build();
+    }
+
+    public Equipamento toEntityUpdate(Equipamento equipamento) {
+        return Equipamento.builder()
+                .id(equipamento.getId())
+                .tipo(this.tipo)
+                .defeito(this.defeito)
+                .parecer(this.parecer)
+                .build();
     
+}
 }
 
 //olá
