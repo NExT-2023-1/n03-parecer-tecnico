ddcabcf5642a9212ee56630ff4b84f5efa932a9f
Luiz Diniz <luizdinizmedeiros@gmail.com>
2023-09-21 10:39:31 -0300

    equipamantoService

--- src/main/java/CESAR/NExT/ParecerTecnicoAPI/services/EquipamentoService.java
+++ src/main/java/CESAR/NExT/ParecerTecnicoAPI/services/EquipamentoService.java
@@ -1,5 +1,58 @@
 package CESAR.NExT.ParecerTecnicoAPI.services;
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import CESAR.NExT.ParecerTecnicoAPI.dto.EquipamentoDTO;
+import CESAR.NExT.ParecerTecnicoAPI.entities.Equipamento;
+import CESAR.NExT.ParecerTecnicoAPI.repositories.EquipamentoRepository;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+
+
+
 public class EquipamentoService {
+      private final EquipamentoRepository equipamentoRepository;
+
+    public Equipamento create(EquipamentoDTO equipamentoDTO) {
+        Equipamento equipamento = equipamentoDTO.toEntity();
+        return this.equipamentoRepository.save(equipamento);
+    }
+
+    public List<Equipamento> listAll() {
+        List<Equipamento> equipamentos = equipamentoRepository.findAll();
+        return equipamentos.stream()
+                .collect(Collectors.toList());
+    }
+
+    public Equipamento getById(long id) {
+        return this.equipamentoRepository.findById(id).orElse(null);
+    }
+
+    public Equipamento update(long id, @Valid EquipamentoDTO equipamentoDTO) {
+        Equipamento client = this.equipamentoRepository.findById(id).orElse(null);
+        if (client != null){
+            Equipamento updateClient = equipamentoDTO.toEntityUpdate(client);
+            return this.equipamentoRepository.save(updateClient);
+        }
+        return null;
+    }
+
+    public boolean delete(long id) {
+        Equipamento client = this.equipamentoRepository.findById(id).orElse(null);
+        if (client != null){
+            this.equipamentoRepository.delete(client);
+            return true;
+        }
+        return false;
+    }
+
     
 }
