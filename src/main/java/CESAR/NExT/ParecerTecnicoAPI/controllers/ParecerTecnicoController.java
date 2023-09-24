996328a593b3006f5d241bd47415e31261976ef6
Eduardo Morais <dudu.vgvm@gmail.com>
2023-09-24 13:46:01 -0300

    Projeto Final

--- src/main/java/CESAR/NExT/ParecerTecnicoAPI/controllers/ParecerTecnicoController.java
+++ src/main/java/CESAR/NExT/ParecerTecnicoAPI/controllers/ParecerTecnicoController.java
@@ -4,15 +4,23 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import CESAR.NExT.ParecerTecnicoAPI.dto.ParecerTecnicoDTO;
 import CESAR.NExT.ParecerTecnicoAPI.entities.ParecerTecnico;
 import CESAR.NExT.ParecerTecnicoAPI.services.ExcelService;
 import CESAR.NExT.ParecerTecnicoAPI.services.ParecerTecnicoService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -22,6 +30,40 @@ public class ParecerTecnicoController {
     
     private final ParecerTecnicoService parecerTecnicoService;
     
+    @GetMapping
+    public ResponseEntity<List<ParecerTecnico>> listAll() {
+        List<ParecerTecnico> parecerList = this.parecerTecnicoService.listAll();
+        return new ResponseEntity<>(parecerList, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<ParecerTecnico> create(@RequestBody @Valid ParecerTecnicoDTO parecerTecnicoDTO) {
+        ParecerTecnico parecerTecnico = parecerTecnicoService.create(parecerTecnicoDTO);
+        return new ResponseEntity<>(parecerTecnico, HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ParecerTecnico> getById(@PathVariable long id) {
+        ParecerTecnico parecerTecnico = this.parecerTecnicoService.getById(id);
+        if (parecerTecnico != null){
+            return new ResponseEntity<>(parecerTecnico, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ParecerTecnico> update(@PathVariable long id, @RequestBody @Valid ParecerTecnicoDTO parecerTecnicoDTO) {
+        ParecerTecnico parecerTecnico = parecerTecnicoService.update(id, parecerTecnicoDTO);
+        if (parecerTecnico != null){
+            return new ResponseEntity<>(parecerTecnico, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        if (parecerTecnicoService.delete(id)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
     @GetMapping("/excelFile")
     public void exportToExcel(HttpServletResponse response) throws IOException {
         response.setContentType("application/octet-stream");
